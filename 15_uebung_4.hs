0f1b05371889aa004e05177ed8eb7d4fb4954345
Mario Kerndler <mariokerndler@gmail.com>
2023-11-12 00:04:41 +0100

    UE4

--- 15_uebung_4.hs
+++ 15_uebung_4.hs
@@ -3,6 +3,10 @@
 {-# HLINT ignore "Use camelCase" #-}
 module UE4 where
 
+import Data.Foldable (find)
+import Data.List (nubBy)
+import Distribution.Simple.Program.HcPkg (register)
+
 ------------------------------------------------------------------------------------
 -- A1
 ------------------------------------------------------------------------------------
@@ -147,6 +151,8 @@ type Fuehrerscheinklasse = String
 
 type Gemeinde = String
 
+type Wahrheitswert = Bool
+
 data Person = Person
   { personenName :: Name,
     geschlecht :: Geschlecht,
@@ -159,6 +165,51 @@ data Person = Person
   deriving (Show, Eq)
 
 type Personenregister = [Person]
+
+bereinige_Personenregister :: Personenregister -> Personenregister
+bereinige_Personenregister = nubBy (\p1 p2 -> ausweisnummer p1 == ausweisnummer p2)
+
+ist_valides_Personenregister :: Personenregister -> Bool
+ist_valides_Personenregister register = all (istEindeutig register . ausweisnummer) register
+
+istEindeutig :: Personenregister -> Personalausweisnummer -> Bool
+istEindeutig register anum = length (filter (\p -> ausweisnummer p == anum) register) == 1
+
+ist_wohnhaft_in :: Personenregister -> Personalausweisnummer -> Gemeinde -> Wahrheitswert
+ist_wohnhaft_in register anum gemeinde =
+  any (\p -> anum == ausweisnummer p && gemeinde `elem` anschriften p) register
+
+einwohner_von :: Personenregister -> Gemeinde -> [(Name, Geschlecht, Alter)]
+einwohner_von register gemeinde =
+  [(personenName p, geschlecht p, alter p) | p <- register, gemeinde `elem` anschriften p]
+
+durchschnittsalter_mit_Geschlecht_in :: Personenregister -> Geschlecht -> Gemeinde -> Alter
+durchschnittsalter_mit_Geschlecht_in register gesch gemeinde =
+  let relevantePersonen = filter (\p -> gemeinde `elem` anschriften p && gesch == geschlecht p) register
+      totalAge = sum (map alter relevantePersonen)
+      totalCount = length relevantePersonen
+   in if totalCount == 0 then 0 else totalAge `div` totalCount
+
+haben_ausschliesslich_als_Wohnsitz :: Personenregister -> Anschrift -> [Person]
+haben_ausschliesslich_als_Wohnsitz register anschrift =
+  filter (\p -> length (anschriften p) == 1 && head (anschriften p) == anschrift) register
+
+familienstand_von :: Personenregister -> Personalausweisnummer -> Familienstand
+familienstand_von register anum =
+  maybe
+    "Unbekannt"
+    familienstand
+    (find (\p -> anum == ausweisnummer p) register)
+
+haben_Lenkberechtigung_fuer :: Personenregister -> Fuehrerscheinklasse -> [(Name, Personalausweisnummer)]
+haben_Lenkberechtigung_fuer register klasse =
+  [(personenName p, ausweisnummer p) | p <- register, klasse `elem` fuehrerscheinklassen p]
+
+ummelden :: Personenregister -> Anschrift -> Anschrift -> Personenregister
+ummelden register von nach =
+  map (\p -> if von `elem` anschriften p then p {anschriften = replace von nach (anschriften p)} else p) register
+  where
+    replace old new = map (\x -> if x == old then new else x)
 
 ------------------------------------------------------------------------------------
 -- Hilfsfunktionen
